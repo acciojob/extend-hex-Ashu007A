b086e7ce5c85d789860d886ee9168cda22c2f7ae
Ashish Kumar Ram <74091038+Ashu007A@users.noreply.github.com>
2023-11-30 14:44:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const extendHex = (shortHex) => {
   // write your code here
+	shortHex = shortHex.replace("#", "").toLowerCase();
+
+    // Ensure the input is a valid short hex code
+    if (!/^[0-9a-f]{3}$/.test(shortHex)) {
+        alert("Invalid short hex code. Please enter a valid short hex code.");
+        return;
+    }
+
+    // Expand the short hex code to the full hex code
+    const fullHex = shortHex
+        .split("")
+        .map((char) => char.repeat(2))
+        .join("");
+
+    return `#${fullHex}`;
 };
 
 // Do not change the code below.
